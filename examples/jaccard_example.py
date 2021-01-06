325196a059d7440be48991020bef41bc971456ad
durgeshsamariya <samariya.durgesh@gmail.com>
2021-01-06 10:28:07 +0530

    Example of using Jaccard distance measure.

---
+++ examples/jaccard_example.py
@@ -0,0 +1,24 @@
+"""
+    author: Durgesh Samariya
+    email: samariya.durgesh@gmail.com
+    Licence: BSD 2 clause
+    Description: Example of using Jaccard distance measure.
+"""
+
+import numpy as np
+import os
+import sys
+
+# temporary solution for relative imports in case pyod is not installed
+# if pysim is installed, no need to use the following line
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname("__file__"), '..')))
+
+from pysim.distances import jaccard
+
+if __name__ == "__main__":
+    a = np.array([1, 2, 43])
+    b = np.array([3, 2, 1])
+
+    jaccard_dist = jaccard(a, b)
+    print('Jaccard distance is : {}'.format(jaccard_dist))
